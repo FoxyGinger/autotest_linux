e6ba5e4bd55d80acfb853657ecf84b7a7298c06e
FoxyGinger <electrojanet@gmail.com>
2023-11-12 20:58:03 +0300

    homework 1 fixes after review

--- command_func.py
+++ command_func.py
@@ -2,9 +2,8 @@ import subprocess
 
 
 def execute_func(cmd: str, expected_str: str) -> bool:
-    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
-    out = result.stdout.decode(encoding='utf-8')
-    if expected_str in out:
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    if result.returncode == 0 and expected_str in result.stdout:
         return True
 
     return False
