4db9c6b3c92092cb53555431bb7486a2f265a943
FoxyGinger <electrojanet@gmail.com>
2023-11-16 10:32:37 +0300

    homework 4: tests clean up

--- seminar4/test_negative.py
+++ seminar4/test_negative.py
@@ -9,12 +9,10 @@ with open('config.yaml') as f:
 
 class TestNegative:
     def test_step1(self, make_bad_arx):
-        assert ssh_checkout_negative("0.0.0.0", "user2", "2222",
-                                     "cd {}; 7z e bad_arx.7z -o{} -y".format(data["folder_out"],
+        assert ssh_checkout_negative("cd {}; 7z e bad_arx.7z -o{} -y".format(data["folder_out"],
                                                                              data["folder_ext"]),
                                      "ERRORS"), "Test1 FAIL"
 
     def test_step2(self):
         # test2 =========show info about arx2.7z
-        assert ssh_checkout_negative("0.0.0.0", "user2", "2222",
-                                     "cd {}; 7z t bad_arx.7z".format(data["folder_out"]), "ERRORS"), "Test2 FAIL"
+        assert ssh_checkout_negative("cd {}; 7z t bad_arx.7z".format(data["folder_out"]), "ERRORS"), "Test2 FAIL"
